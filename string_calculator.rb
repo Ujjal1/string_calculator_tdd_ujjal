605a0e1537b1ac97bd7cb300b8df55222612680b
root <root@LAPTOP-DG32HQQB>
2025-01-11 11:56:45 +0530

    Added code for different delimeter support

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,6 +5,12 @@ class StringCalculator
 
     delimiter = ','
 
+    if numbers.start_with?('//')
+      parts = numbers.split("\n", 2)
+      delimiter = parts[0][2..-1]
+      numbers = parts[1]
+    end
+
     number_list = numbers.split(delimiter)
 
     numbers.gsub!("\n", delimiter)
