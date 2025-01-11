3a417cc0a4b31b5e2704947ee3d00c0a95867121
root <root@LAPTOP-DG32HQQB>
2025-01-11 11:55:01 +0530

    Added code for handling new line between numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,14 @@
 class StringCalculator
+  
   def self.add(numbers)
     return 0 if numbers.empty?
+
     delimiter = ','
+
     number_list = numbers.split(delimiter)
+
+    numbers.gsub!("\n", delimiter)
     number_list.map(&:to_i).reduce(0, :+)
   end
+
 end
