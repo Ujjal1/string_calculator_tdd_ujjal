7e4b69c764bdf7e7b61c83b6fca8a7d59d05d92c
root <root@LAPTOP-DG32HQQB>
2025-01-11 11:57:50 +0530

    Added test case for negative numbers

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -27,5 +27,13 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("//;\n1;2")).to eq(3)
       expect(StringCalculator.add("//|\n1|2|3")).to eq(6)
     end
+
+    it 'raises an exception for negative numbers' do
+      expect { StringCalculator.add('1,-2,3') }.to raise_error('negative numbers not allowed: -2')
+    end
+    
+    it 'shows all negative numbers in the exception message' do
+      expect { StringCalculator.add('-1,-2,3') }.to raise_error('negative numbers not allowed: -1, -2')
+    end
   end
 end
