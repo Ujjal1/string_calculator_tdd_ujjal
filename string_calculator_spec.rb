294ebeb5585dcfad6887dcf4fd40edd2346b136d
root <root@LAPTOP-DG32HQQB>
2025-01-11 11:47:15 +0530

    Added test cases for one and two numbers

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add('')).to eq(0)
     end
+
+    it 'returns the number for a single number' do
+        expect(StringCalculator.add('1')).to eq(1)
+        expect(StringCalculator.add('5')).to eq(5)
+      end
+      it 'returns the sum of two numbers' do
+        expect(StringCalculator.add('1,5')).to eq(6)
+      end
   end
 end
